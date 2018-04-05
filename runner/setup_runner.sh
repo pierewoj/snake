9285a0a7491ed46b6197fd5e268d3765128979cb
kuba <kuba@pierewoj.com>
2018-04-05 23:26:33 +0200

    Add runner

--- runner/setup_runner.sh
+++ runner/setup_runner.sh
@@ -1,4 +1,10 @@
 sudo yum update
-sudo yum install python redis-cli
+sudo yum install python gcc make
 pip install --upgrade pip
 pip install redis
+
+# installing redis
+wget http://download.redis.io/redis-stable.tar.gz
+tar xvzf redis-stable.tar.gz
+cd redis-stable
+make
